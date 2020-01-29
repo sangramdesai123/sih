fa71bd6a9b0c296e068111f6d7a3c0bf2d422b0a
sangramdesai123 <sangram.desai@somaiya.edu>
2020-01-30 00:16:44 +0530

    firebase integration with history page

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template
-
+import pyrebase
+from config import *
 # create the application object
 app = Flask(__name__)
+
+firebase = pyrebase.initialize_app(con)
+db = firebase.database()
 
 @app.route('/')
 def home():
@@ -10,11 +14,21 @@ def home():
 
 @app.route('/history')
 def history():
-    return render_template('history.html')  # render a history template
+	values = list(db.get().val().values())
+	print(values)	
+	arr = []
+	for i in range(len(values)):
+		arr.append([values[i]["author"],values[i]["date"],values[i]["location"]["road"]+","+values[i]["location"]["city"],values[i]["pci"],values[i]["time"]])
+	print(arr)
+	return render_template('history.html',values=arr)
 
 @app.route('/report')
 def report():
-    return render_template('report.html')  # render a  report template
+	
+	return render_template('report.html')
+
+
+#db.child("roadlyser").push({"author":"gole"})
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
